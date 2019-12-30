13dad5ce8d550bfc5036cae8f7b21c0ef435b4c8
RecodeExistence <recodeexistance@gmail.com>
2019-12-30 16:24:02 +0000

    created buzzword prop array, changed banner style and background to more neutral

---
+++ reactfolio/src/components/BuzzWords.js
@@ -0,0 +1,3 @@
+//buzzwords array, can be added to, edited, or stored on a backend and fetched. 
+
+export const buzzWords = ['Coder', 'Thinker', 'Software Engineer', 'React fan!', 'problem solver'];
