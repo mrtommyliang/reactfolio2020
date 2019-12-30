a985722e42a236da01dcd0f314993c5a196aab76
Tommy Liang <tommyliangsays@gmail.com>
2019-12-30 11:08:08 -0500

    working on toggleable burger menu

--- reactfolio/src/Components/Navbar.jsx
+++ reactfolio/src/Components/Navbar.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import '../css/navbar.css'
+import navbarmenu from '../resources/bars-solid.svg'
 
 export default function Navbar() {
   return (
     <div>
       <nav className="navbar"/>
-      <span class="navbar-toggle" id="js-navbar-toggle">
-        <i class="fas fa-bars"></i>
-      </span>
+        <span class="navbar-toggle" id="js-navbar-toggle">
+          <img className="burgermenu" src={navbarmenu} alt="burger"/>
+        </span>
         <a href="#" className="logo">Tommy Liang</a>
         <ul class="main-nav" id="navbar-container">
           <li>
