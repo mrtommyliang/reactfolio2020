6b5a7d4bcdee9387d8a86f827d35e5faddde537f
RecodeExistence <recodeexistance@gmail.com>
2019-12-30 15:03:53 +0000

    created HeroBanner.js and .css.  Started working the styling, static data atm.

---
+++ reactfolio/src/components/HeroBanner.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../css/HeroBanner.css';
+
+const HeroBanner = () => {
+    
+        return (
+            <div className = "heroBanner">
+                <h1>TommyLiang ReactFolio</h1>
+                
+                <div className = "softwareEngineerDetails">
+                    <img src = 'https://picsum.photos/100' alt = 'lorem picsum' />
+                    <p>Tommy Liang is a: </p>
+
+                </div>
+            </div>
+        )
+    
+}
+
+export default HeroBanner;
