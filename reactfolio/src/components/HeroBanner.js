ef7d48741ed66a2eaffadce9bc650b2ed5996380
RecodeExistence <recodeexistance@gmail.com>
2019-12-30 16:35:02 +0000

    corrected stash issue.  background and page neutral, basic array prop started for typewriter function.

--- reactfolio/src/components/HeroBanner.js
+++ reactfolio/src/components/HeroBanner.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import '../css/HeroBanner.css';
-
+import { buzzWords } from './BuzzWords'
 const HeroBanner = () => {
-    
+ 
+    const buzzWordSelect = () => {
+        buzzWords.forEach(word => word);
+    }
         return (
             <div className = "heroBanner">
                 <h1>TommyLiang ReactFolio</h1>
@@ -10,7 +13,7 @@ const HeroBanner = () => {
                 <div className = "softwareEngineerDetails">
                     <img src = 'https://picsum.photos/100' alt = 'lorem picsum' />
                     <p>Tommy Liang is a: </p>
-
+                        <div className = "buzzWordDisplay">{buzzWordSelect}</div>
                 </div>
             </div>
         )
